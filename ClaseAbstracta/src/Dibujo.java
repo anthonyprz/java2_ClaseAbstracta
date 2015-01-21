b18b1bb77a1c794c066102b98a909ee4b959a759
anthony <thony_prz@outlook.com>
2015-01-21 18:10:25 +0100

    Update Dibujo.java

--- ClaseAbstracta/src/Dibujo.java
+++ ClaseAbstracta/src/Dibujo.java
@@ -2,7 +2,7 @@
 public class Dibujo extends FiguraGeometrica {
 
 	@Override
-	void dibujar() {
+	public void dibujar() {
 		// TODO Auto-generated method stub
 		System.out.println("estoy dibujando un perro");
 	}
