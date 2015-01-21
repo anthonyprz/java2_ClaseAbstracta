be8c310ecaf5a4216972675151d71c8e220a14c9
Sonia Ortiz de Arri <soniaodea@gmail.com>
2015-01-21 13:39:49 +0100

    clase abstracta lista

---
+++ ClaseAbstracta/src/Dibujo.java
@@ -0,0 +1,10 @@
+
+public class Dibujo extends FiguraGeometrica {
+
+	@Override
+	void dibujar() {
+		// TODO Auto-generated method stub
+		System.out.println("estoy dibujando un perro");
+	}
+	
+}
---
+++ ClaseAbstracta/src/FiguraGeometrica.java
@@ -0,0 +1,27 @@
+/*
+Es una clases cuya descripción es incompleta. Una clase abstracta declara métodos, pero no
+tiene que implementarlos. 
+	
+	No proporcionan la implementación de todos sus métodos
+	Los métodos no implementados se declaran como abstract
+	Una clase con un método abstracto debe declararse como clase abstracta
+	Pero una clase puede declararse como abstracta aunque no tenga ningún método
+	abstracto
+
+		los métodos abstactos no tienen cuerpo {}
+		 
+		 Las subclases de una clase abstracta deben:
+ 			Sobreescribir todos los métodos abstractos de la superclase, o bien
+ 			Ser declaradas como clases abstractas
+ 		Una clase abstracta no puede instanciarse
+			No se pueden crear objetos de una clase abstracta
+		Una clase abstracta puede incluir variables y métodos no abstractos.
+ 		No se pueden definir constructores abstractos o métodos estáticos abstractos.
+
+		
+		
+		
+*/
+public abstract class FiguraGeometrica {
+	 abstract void dibujar();
+}
